ad558e909f81084641d3ea1ce8da83ded1787f81
SeongHo-C <leeseong7130@gmail.com>
2025-05-07 10:29:05 +0900

    Feat: 표적지 모서리 좌표 입력(수동, 자동)

--- backend/plc_controller.py
+++ backend/plc_controller.py
@@ -13,7 +13,7 @@ class PlcController:
 
     def manual_shoot(self, mode, x, y):
         if mode == 'PLC':
-            # 연속된 주소(1500, 1501)로 변경 희망
+            # 연속된 주소(1500, 1501)로 변경 희망, 하나의 주소만을 쓰더라도 사격이 되는지 확인 필요
             # self.client.write_multiple_registers(1500, [int(x), int(y)])
 
             """
@@ -24,16 +24,3 @@ class PlcController:
                 print('PLC 좌표 쓰기 실패')
             """
             print(f'PLC 좌표 쓰기 성공: {x}, {y}')
-
-
-def shoot_plc(self, plc_x, plc_y):
-    # 1. X, Y 값 모두 먼저 기록
-    x_result = self.client.write_single_register(1500, int(plc_x))
-    y_result = self.client.write_single_register(1600, int(plc_y))
-
-    # 2. 둘 다 성공했을 때 트리거 신호
-    if x_result and y_result:
-        self.client.write_single_register(1700, 1)  # 트리거 신호
-        print(f'PLC: X축 {plc_x}, Y축 {plc_y} 사격 트리거')
-    else:
-        print('PLC: 좌표 쓰기 실패')
