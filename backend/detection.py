ebefe4746c4f834001f81ce966c5ebdf9b85409c
SeongHo-C <leeseong7130@gmail.com>
2025-05-21 09:38:03 +0900

    Feat: 말벌 탐지 결과 확인

--- backend/detection.py
+++ backend/detection.py
@@ -10,13 +10,36 @@ class Detection:
         self.model = YOLO(model_path)
         self.model.to(self.device)
 
+        self.class_names = self.model.names
+        print(self.class_names)
+
     def detect(self, frame):
         results = self.model.track(
             source=frame,
             tracker='botsort.yaml',
             conf=0.3,  # 검출 신뢰도 임계값. 높이면 오탐↓, 미탐↑
-            iou=0.6,  # NMS에서 겹침 허용치. 낮추면 중복↓, 높이면 중복↑
+            iou=0.5,  # NMS에서 겹침 허용치. 낮추면 중복↓, 높이면 중복↑
             persist=True,
             verbose=False
         )
+
+        detections = self.parse_results(results)
+        print(detections)
+
         return results[0].plot()
+
+    def parse_results(self, results):
+        detections = []
+
+        for box in results[0].boxes:
+            cls_id = int(box.cls.item())
+            bbox = box.xyxy[0].tolist()
+            track_id = int(box.id.item()) if box.id is not None else None
+
+            detections.append({
+                'class': self.class_names[cls_id],
+                'bbox': bbox,
+                'track_id': track_id
+            })
+
+        return detections
